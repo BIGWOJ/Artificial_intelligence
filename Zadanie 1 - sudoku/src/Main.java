2a2dcc0d11f83fb083f2cf61c4b48813f1cb8bac
lw53894 <lw53894@zut.edu.pl>
2024-10-08 21:41:33 +0200

    ''

--- Zadanie 1 - sudoku/src/Main.java
+++ Zadanie 1 - sudoku/src/Main.java
@@ -1,34 +1,55 @@
+import sac.graph.GraphState;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Sudoku sudoku_test = new Sudoku(3);
 
         //Losowe liczby w sudoku
 //        StringBuilder random_numbers = new StringBuilder();
 //        Random random_number = new Random();
 //        for (int i = 0; i < 81; i++){
-//            random_numbers.append(random_number.nextInt(1, 10));
+//            //random_numbers.append(random_number.nextInt(1,10));
+//            random_numbers.append(random_number.nextInt(9)+1);
 //        }
 //
 //        sudoku_test.fromString(random_numbers.toString());
 //        String sudoku_numbers = sudoku_test.toString();
 //        System.out.println(sudoku_numbers);
-//        System.out.println(sudoku_test.isValid() + "\n");
 
 
         //Poprawne liczby w sudoku
-        String correct_sudoku = "123456789234567891345678912456789123567891234678912345789123456891234567912345678";
-        sudoku_test.fromString(correct_sudoku);
-        String correct_sudoku_numbers = sudoku_test.toString();
-        System.out.println(correct_sudoku_numbers);
-        System.out.println(sudoku_test.isValid() + "\n");
+//        String correct_sudoku =
+//                "123456789" +
+//                        "234567891" +
+//                        "345678912" +
+//                        "456789123" +
+//                        "567891234" +
+//                        "678912345" +
+//                        "789123456" +
+//                        "891234567" +
+//                        "912345678";
+//
+//        sudoku_test.fromString(correct_sudoku);
+//        String correct_sudoku_numbers = sudoku_test.toString();
+//        System.out.println(correct_sudoku_numbers);
 
+
+        String test = "7.9..4.....4958..........5.34..1.....2...3....91.7.84.....82.......4.3..81...7..2";
+        sudoku_test.fromString(test);
+
+        System.out.println("Sudoku poprawne?: " + sudoku_test.isValid() + "\n");
+        System.out.println(sudoku_test.toString());
         System.out.println("Liczba niewiadomych: " + sudoku_test.unknown_counter());
+        //System.out.println(sudoku_test.generateChildren());
+
+
+
 
     }
 }
--- Zadanie 1 - sudoku/src/Sudoku.java
+++ Zadanie 1 - sudoku/src/Sudoku.java
@@ -1,16 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.math.*;
+import java.util.List;
+import java.util.function.DoubleToIntFunction;
 
-public class Sudoku {
+import sac.*;
+import sac.graph.GraphState;
+import sac.graph.GraphStateImpl;
+
+public class Sudoku extends GraphStateImpl {
     private char[][] board;
+    int unknowns;
+
+    @Override
+    public boolean isSolution (){
+        return 0 == unknown_counter();
+    }
+
+    @Override
+    public List<GraphState> generateChildren() {
+        List<GraphState> lst = new ArrayList<>();
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                if (!Character.isDigit(board[i][j])){
+                    for (int k = 1; k <= board.length; k++){
+                        board[i][j] = (char) (k+'0');
+                        unknowns--;
+                        if (isValid()){
+                            lst.add(new Sudoku(this));
+                        }
+                        unknowns++;
+                        board[i][j] = '.';
+                    }
+                    return lst;
+                }
+            }
+        }
+        return lst;
+    }
+
+    public Sudoku(Sudoku state){
+        super();
+        unknowns = state.unknowns;
+        board = new char[state.board.length][];
+        for (int i = 0; i < board.length; i++){
+            board[i] = java.util.Arrays.copyOf(state.board[i], board[i].length);
+        }
+    }
 
     public Sudoku(int n) {
         board = new char[n * n][n * n];
+        unknowns = n*n*n*n;
     }
 
     public void fromString(String numbers) {
         if (numbers.length() != 81){
-            System.out.println("Ilosc cyfr jest niepoprawna.");
-            return;
+            System.out.println("Ilość cyfr jest niepoprawna. Jest " + numbers.length() + " zamiast 81.");
+            System.exit(1);
         }
         //System.out.println(board.length);
         board = new char[9][9];
@@ -19,6 +66,7 @@ public class Sudoku {
                 board[i][j] = numbers.charAt(i * 9 + j);
             }
         }
+        unknowns = unknown_counter();
     }
 
     @Override
@@ -34,17 +82,57 @@ public class Sudoku {
     }
 
     boolean isValid() {
+        // Sprawdzanie wierszy
         HashSet<Character> row = new HashSet<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 char current = board[i][j];
-                if (row.contains(current) || !Character.isDigit(current)) {
+                if (row.contains(current) && Character.isDigit(current)) {
                     return false;
                 }
                 row.add(current);
             }
             row.clear();
         }
+
+        // Sprawdzanie kolumn
+        HashSet<Character> column = new HashSet<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                char current = board[j][i];
+                if (column.contains(current) && Character.isDigit(current)) {
+                    return false;
+                }
+                column.add(current);
+            }
+            column.clear();
+        }
+
+        // Sprawdzanie małych kwadratów
+        int n = (int) Math.sqrt(board.length); // n będzie równe 3 dla Sudoku 9x9
+
+        for (int blockRow = 0; blockRow < n; blockRow++) {
+            for (int blockCol = 0; blockCol < n; blockCol++) {
+                HashSet<Character> mini_square = new HashSet<>();
+                System.out.println(blockRow);
+                // Iteracja po komórkach w danym kwadracie 3x3
+                for (int i = 0; i < n; i++) { // 0, 1, 2
+                    for (int j = 0; j < n; j++) { // 0, 1, 2
+                        //System.out.println(board[i][j]);
+                        char current = board[blockRow * n + i][blockCol * n + j];
+
+                        // Sprawdź, czy obecny znak to cyfra i czy już istnieje w mini kwadracie
+                        if (Character.isDigit(current)) {
+                            if (mini_square.contains(current)) {
+                                return false; // Znalazłem duplikat
+                            }
+                            mini_square.add(current); // Dodaj do zbioru
+                        }
+                    }
+                }
+            }
+        }
+
         return true;
     }
 
