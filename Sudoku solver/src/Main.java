91cdd52455f19a5a32ccf5e92724028a56e5876b
Wojciech Latos <lw53894>
2024-10-22 17:16:13 +0200

    Comments added

--- Sudoku solver/src/Main.java
+++ Sudoku solver/src/Main.java
@@ -3,34 +3,38 @@ import sac.graph.GraphSearchConfigurator;
 
 public class Main {
     public static void main(String[] args) {
+        //Testing sudoku
         String sudokuTest = "...9348....78.............9..8.....6.2....7.........4.94.38..7.6....9.2.2.5.6.3.4";
+        //Creating new sudoku board
         Sudoku sudoku = new Sudoku(3);
 
         sudoku.fromString(sudokuTest);
 
-        System.out.println("Sudoku poprawne? " + sudoku.isValid());
-        System.out.println("Liczba niewiadomych: " + sudoku.unknown_counter());
-        System.out.println("Początkowe sudoku:\n" + sudoku);
+        System.out.println("Is sudoku correct? " + sudoku.isValid());
+        System.out.println("Empty tiles: " + sudoku.unknown_counter());
+        System.out.println("Starting sudoku:\n" + sudoku);
 
+        //Setting number of solutions returning from searching algorithm
         GraphSearchConfigurator configurator = new GraphSearchConfigurator();
         configurator.setWantedNumberOfSolutions(Integer.MAX_VALUE);
 
+        //Giving sudoku to BFS algorithm to solve it
         Sudoku.setHFunction(new Heurystyka());
         BestFirstSearch bfs = new BestFirstSearch(sudoku, configurator);
         bfs.setInitial(sudoku);
         bfs.execute();
 
-        int liczba_rozwiazan = bfs.getSolutions().size();
-        System.out.println("Liczba rozwiązań: " + liczba_rozwiazan);
-        if (liczba_rozwiazan == 1) {
-            System.out.println("Rozwiązanie:\n" + bfs.getSolutions());
+        int solution_numbers = bfs.getSolutions().size();
+        System.out.println("Number of solutions: " + solution_numbers);
+        if (solution_numbers == 1) {
+            System.out.println("Solution:\n" + bfs.getSolutions());
         }
         else {
-            System.out.println("Przykładowe rozwiązanie:\n" + bfs.getSolutions().get(0));
+            System.out.println("Example solution:\n" + bfs.getSolutions().get(0));
         }
 
-        System.out.println("Liczba stanów w zbiorze Open: " + bfs.getOpenSet().size());
-        System.out.println("Liczba stanów w zbiorze Closed: " + bfs.getClosedSet().size());
-        System.out.println("Czas pracy: " + bfs.getDurationTime() + "ms");
+        System.out.println("Number of states in Open: " + bfs.getOpenSet().size());
+        System.out.println("Number of states in Closed: " + bfs.getClosedSet().size());
+        System.out.println("Working time: " + bfs.getDurationTime() + "ms");
     }
 }
--- Sudoku solver/src/Sudoku.java
+++ Sudoku solver/src/Sudoku.java
@@ -5,39 +5,45 @@ import sac.*;
 import sac.graph.GraphState;
 import sac.graph.GraphStateImpl;
 
-
 public class Sudoku extends GraphStateImpl {
     private char[][] board;
     int unknowns;
 
+    //Checking if sudoku is solved
     @Override
     public boolean isSolution (){
         return 0 == unknown_counter();
     }
 
+    //Generating children representing different paths of trying to solve sudoku
     @Override
     public List<GraphState> generateChildren() {
         List<GraphState> lst = new ArrayList<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
+                //Assuming the tile is empty when there is something other than digit
                 if (!Character.isDigit(board[i][j])) {
+                    //Generating children with every variant of numbers to solve sudoku...
                     for (int k = 1; k <= board.length; k++) {
                         board[i][j] = (char) (k + '0');
                         unknowns--;
+                        //... and adding to returning list
                         if (isValid()) {
                             lst.add(new Sudoku(this));
                         }
                         unknowns++;
                         board[i][j] = '.';
                     }
+                    //Returning list of potential solving variants (generated children)
                     return lst;
                 }
             }
         }
+        //If no empty tile was found - return the current list
         return lst;
     }
 
-
+    //Copy constructor using while generating children
     public Sudoku(Sudoku state) {
         super();
         this.unknowns = state.unknowns;
@@ -45,19 +51,22 @@ public class Sudoku extends GraphStateImpl {
         for (int i = 0; i < board.length; i++) {
             this.board[i] = java.util.Arrays.copyOf(state.board[i], state.board[i].length);
         }
-}
+    }
 
+    //Sudoku constructor
     public Sudoku(int n) {
         board = new char[n * n][n * n];
         unknowns = n*n*n*n;
     }
 
+    //Creating sudoku from given string e.g. "..4.94." which should be 81 long (9x9 sudoku).
+    //"." representing empty tile
     public void fromString(String numbers) {
         if (numbers.length() != 81){
-            System.out.println("Ilość cyfr jest niepoprawna. Jest " + numbers.length() + " zamiast 81.");
+            System.out.println("Number of digits is not correct. There are " + numbers.length() + " instead of 81.");
             System.exit(1);
         }
-        //System.out.println(board.length);
+
         board = new char[9][9];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
@@ -67,6 +76,7 @@ public class Sudoku extends GraphStateImpl {
         unknowns = unknown_counter();
     }
 
+    //Representing sudoku as text in console
     @Override
     public String toString() {
         StringBuilder text = new StringBuilder();
@@ -79,8 +89,9 @@ public class Sudoku extends GraphStateImpl {
         return text.toString();
     }
 
+    //Checking if sudoku is valid (different digits in row, column and mini squares (3x3))
     boolean isValid() {
-        // Sprawdzanie wierszy
+        //Rows validation
         for (int i = 0; i < board.length; i++) {
             HashSet<Character> row = new HashSet<>();
             for (int j = 0; j < board[i].length; j++) {
@@ -92,7 +103,7 @@ public class Sudoku extends GraphStateImpl {
             }
         }
 
-        // Sprawdzanie kolumn
+        //Columns validation
         for (int i = 0; i < board.length; i++) {
             HashSet<Character> column = new HashSet<>();
             for (int j = 0; j < board[i].length; j++) {
@@ -104,7 +115,7 @@ public class Sudoku extends GraphStateImpl {
             }
         }
 
-        // Sprawdzanie małych kwadratów
+        //Mini squares validation
         int n = (int) Math.sqrt(board.length);
         for (int blockRow = 0; blockRow < n; blockRow++) {
             for (int blockCol = 0; blockCol < n; blockCol++) {
@@ -123,6 +134,7 @@ public class Sudoku extends GraphStateImpl {
         return true;
     }
 
+    //Checking how many tiles are empty
     public int unknown_counter(){
         int counter = 0;
         for (int i = 0; i < board.length; i++) {
@@ -137,11 +149,13 @@ public class Sudoku extends GraphStateImpl {
     }
 }
 
+//Heuristic function
 class Heurystyka extends StateFunction {
     @Override
     public double calculate (State s) {
         if (s instanceof Sudoku) {
             Sudoku ss = (Sudoku) s;
+            //Returning number of empty tiles to help algorithm define the better path to solve
             return ss.unknown_counter();
         }
         else {
