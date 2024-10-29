06aa0aaad6d1422110f7775ac838a052cb7c9a30
BIGWOJ <wojtek.latos@interia.pl>
2024-10-29 12:01:42 +0100

    .

--- Sliding puzzles solver/src/Main.java
+++ Sliding puzzles solver/src/Main.java
@@ -1,11 +1,65 @@
+import sac.graph.BestFirstSearch;
+import sac.graph.GraphSearchConfigurator;
+import sac.graph.GraphState;
 
 public class Main {
     public static void main(String[] args) {
         Puzzle puzzle = new Puzzle(3);
         System.out.println(puzzle);
         System.out.println(puzzle.misplaced_tiles());
+        System.out.println(puzzle.manhattan_distance());
 
-        System.out.println(puzzle);
-        System.out.println(puzzle.misplaced_tiles());
+
+//        String test = "510247863";
+//        puzzle.from_string(test);
+
+//        puzzle.board[0][0] = 5;
+//        puzzle.board[0][1] = 1;
+//        puzzle.board[0][2] = 0;
+//
+//        puzzle.board[1][0] = 2;
+//        puzzle.board[1][1] = 4;
+//        puzzle.board[1][2] = 7;
+//
+//        puzzle.board[2][0] = 8;
+//        puzzle.board[2][1] = 6;
+//        puzzle.board[2][2] = 3;
+
+        System.out.println("Can puzzle be solved? " + puzzle.isSolution());
+        System.out.println("Starting puzzle:\n" + puzzle);
+
+        //Giving puzzle to BFS algorithm to solve it
+        puzzle.setHFunction(new Heurystyka());
+        BestFirstSearch bfs = new BestFirstSearch(puzzle);
+        GraphState shuffled_puzzle = puzzle.shuffle(10);
+        bfs.setInitial(shuffled_puzzle);
+        bfs.execute();
+
+        System.out.println("Number of states in Open: " + bfs.getOpenSet().size());
+        System.out.println("Number of states in Closed: " + bfs.getClosedSet().size());
+
+
+
+
+        //ilość odwiedzonych stanów, czas, closed, open
+
+
+
+
+
+//
+//        int solution_numbers = bfs.getSolutions().size();
+//        System.out.println("Number of solutions: " + solution_numbers);
+//        if (solution_numbers == 1) {
+//            System.out.println("Solution:\n" + bfs.getSolutions());
+//        }
+//        else {
+//            System.out.println("Example solution:\n" + bfs.getSolutions().get(0));
+//        }
+//
+//        System.out.println("Number of states in Open: " + bfs.getOpenSet().size());
+//        System.out.println("Number of states in Closed: " + bfs.getClosedSet().size());
+//        System.out.println("Working time: " + bfs.getDurationTime() + "ms");
     }
+
 }
--- Sliding puzzles solver/src/Puzzle.java
+++ Sliding puzzles solver/src/Puzzle.java
@@ -1,13 +1,16 @@
+import sac.State;
+import sac.StateFunction;
 import sac.graph.GraphState;
 import sac.graph.GraphStateImpl;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class Puzzle extends GraphStateImpl{
+public class Puzzle extends GraphStateImpl {
     public byte[][] board;
     private int empty_tile_row = 0;
     private int empty_tile_column = 0;
-    int misplaced_tiles_counter;
+
 
     @Override
     public List<GraphState> generateChildren() {
@@ -16,9 +19,9 @@ public class Puzzle extends GraphStateImpl{
         //Wiersz inny niż górna krawędź
         if (empty_tile_row != 0) {
             Puzzle puzzle_children = new Puzzle(this);
-            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row-1][empty_tile_column];
+            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row - 1][empty_tile_column];
 
-            puzzle_children.board[empty_tile_row-1][empty_tile_column] = 0;
+            puzzle_children.board[empty_tile_row - 1][empty_tile_column] = 0;
             puzzle_children.empty_tile_row--;
             //Ruch w dół pustego kafelka
             puzzle_children.setMoveName("down");
@@ -28,10 +31,10 @@ public class Puzzle extends GraphStateImpl{
         //Wiersz inny niż dolna krawędź
         if (empty_tile_row != board.length - 1) {
             Puzzle puzzle_children = new Puzzle(this);
-            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row+1][empty_tile_column];
+            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row + 1][empty_tile_column];
 
-            puzzle_children.board[empty_tile_row+1][empty_tile_column] = 0;
-            puzzle_children.empty_tile_row--;
+            puzzle_children.board[empty_tile_row + 1][empty_tile_column] = 0;
+            puzzle_children.empty_tile_row++;
             //Ruch w górę pustego kafelka
             puzzle_children.setMoveName("up");
             children.add(puzzle_children);
@@ -40,10 +43,10 @@ public class Puzzle extends GraphStateImpl{
         //Kolumna inna niż lewa krawędź
         if (empty_tile_column != 0) {
             Puzzle puzzle_children = new Puzzle(this);
-            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row][empty_tile_column-1];
+            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row][empty_tile_column - 1];
 
-            puzzle_children.board[empty_tile_row][empty_tile_column-1] = 0;
-            puzzle_children.empty_tile_row--;
+            puzzle_children.board[empty_tile_row][empty_tile_column - 1] = 0;
+            puzzle_children.empty_tile_column--;
             //Ruch w lewo pustego kafelka
             puzzle_children.setMoveName("left");
             children.add(puzzle_children);
@@ -52,10 +55,10 @@ public class Puzzle extends GraphStateImpl{
         //Kolumna inna niż prawa krawędź
         if (empty_tile_column != board.length - 1) {
             Puzzle puzzle_children = new Puzzle(this);
-            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row][empty_tile_column+1];
+            puzzle_children.board[empty_tile_row][empty_tile_column] = board[empty_tile_row][empty_tile_column + 1];
 
-            puzzle_children.board[empty_tile_row][empty_tile_column+1] = 0;
-            puzzle_children.empty_tile_row--;
+            puzzle_children.board[empty_tile_row][empty_tile_column + 1] = 0;
+            puzzle_children.empty_tile_column++;
             //Ruch w prawo pustego kafelka
             puzzle_children.setMoveName("right");
             children.add(puzzle_children);
@@ -70,10 +73,9 @@ public class Puzzle extends GraphStateImpl{
 
     public Puzzle(int n) {
         board = new byte[n][n];
-        misplaced_tiles_counter = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = (byte) (i*n+j+1);
+                board[i][j] = (byte) (i * n + j);
             }
         }
     }
@@ -86,13 +88,17 @@ public class Puzzle extends GraphStateImpl{
                 board[i][j] = state.board[i][j];
             }
         }
+        this.empty_tile_row = state.empty_tile_row;
+        this.empty_tile_column = state.empty_tile_column;
+        //this.misplaced_tiles_counter = state.misplaced_tiles_counter;
     }
+
 
     int misplaced_tiles() {
         int misplaced_counter = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] != 0 && board[i][j] != (i * board.length + j)+1) {
+                if (board[i][j] != 0 && board[i][j] != (i * board.length + j)) {
                     misplaced_counter++;
                 }
             }
@@ -107,8 +113,8 @@ public class Puzzle extends GraphStateImpl{
             for (int j = 0; j < board[i].length; j++) {
                 int current = board[i][j];
                 if (current != 0) {
-                    int correct_row = (current - 1) / board.length;
-                    int correct_column = (current - 1) % board.length;
+                    int correct_row = current / board.length;
+                    int correct_column = current % board.length;
                     manhattan_distance += Math.abs(i - correct_row) + Math.abs(j - correct_column);
                 }
             }
@@ -127,18 +133,38 @@ public class Puzzle extends GraphStateImpl{
         }
         return text.toString();
     }
+//
+//    public void from_string(String numbers) {
+//        board = new byte[3][3];
+//        for (int i = 0; i < board.length; i++) {
+//            for (int j = 0; j < board.length; j++) {
+//                board[i][j] = (byte) numbers.charAt(i * 3 + j);
+//            }
+//        }
+//    }
 
-    int hash_code(){
+    int hash_code() {
         String board = toString();
         return board.hashCode();
     }
 
-    boolean isValid() {
-        return misplaced_tiles_counter == 0;
+    public GraphState shuffle(int n) {
+        Random r = new Random();
+        GraphState s = this;
+        for (int i = 0; i < n; i++) {
+            List<GraphState> children = s.generateChildren();
+            s = children.get(r.nextInt(children.size()));
+        }
+        return s;
     }
+}
 
-
-
-
-
+class Heurystyka extends StateFunction {
+    @Override
+    public double calculate (State s) {
+        if (s instanceof Puzzle) {
+            return ((Puzzle) s).misplaced_tiles();
+        }
+        else return Double.NaN;
+    }
 }
