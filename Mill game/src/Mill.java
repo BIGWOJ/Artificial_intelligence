3bdf26b491ab44876c3bf0161d66f25560ebe5ef
BIGWOJ <wojtek.latos@interia.pl>
2024-11-10 23:29:28 +0100

    .

--- Mill game/src/Mill.java
+++ Mill game/src/Mill.java
@@ -1,37 +1,116 @@
-import sac.State;
-import sac.StateFunction;
-import sac.game.GameStateImpl;
-import sac.graph.GraphState;
-import sac.graph.GraphStateImpl;
+import java.util.Scanner;
 
-
-public class Mill extends GameStateImpl {
+//public class Mill extends GameStateImpl
+public class Mill {
 
     public char [][] board = new char[3][8];
-    public char player_white = 'W';
-    public char player_black = 'B';
+    public char white_player = 'W';
+    public char black_player = 'B';
 
     public Mill(){
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 8; j++) {
-                board[i][j] = '0';
+                board[i][j] = ' ';
+
+//                StringBuilder position = new StringBuilder();
+//                position.append(i);
+//                position.append(j);
+//                board[i][j] = position.toString().charAt(0);
+//                board[i][j] = position.toString().charAt(1);
+
             }
         }
     }
-
-
-
 
     @Override
     public String toString() {
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 8; j++) {
-                text.append(board[i][j]);
-            }
-            text.append("\n");
-        }
-        return text.toString();
+        StringBuilder mill_board = new StringBuilder();
+        mill_board.append(board[0][6]+"---------------"+board[0][5]+"---------------"+board[0][4]+"\n");
+        mill_board.append("|               |               |\n");
+        mill_board.append("|     "+board[1][6]+"---------"+board[1][5]+"---------"+board[1][4]+"     |\n");
+        mill_board.append("|     |         |         |     |\n");
+        mill_board.append("|     |     "+board[2][6]+"---"+board[2][5]+"---"+board[2][4]+"     |     |\n");
+        mill_board.append("|     |     |       |     |     |\n");
+        mill_board.append(board[0][7]+"-----"+board[1][7]+"-----"+board[2][7]+"       " + board[2][3] + "-----" + board[1][3] + "-----" + board[0][3] + "\n");
+        mill_board.append("|     |     |       |     |     |\n");
+        mill_board.append("|     |     "+board[2][0]+"---"+board[2][1]+"---"+board[2][2]+"     |     |\n");
+        mill_board.append("|     |         |         |     |\n");
+        mill_board.append("|     "+board[1][0]+"---------"+board[1][1]+"---------"+board[1][2]+"     |\n");
+        mill_board.append("|               |               |\n");
+        mill_board.append(board[0][0]+"---------------"+board[0][1]+"---------------"+board[0][2] + "\n");
+        return mill_board.toString();
     }
 
+    public void place_piece(int row, int col, char player){
+        if (board[row][col] != ' '){
+            do {
+                System.out.println("Position already occupied. Place piece elsewhere.");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter row: ");
+                row = scanner.nextInt();
+
+                System.out.println("Enter column: ");
+                col = scanner.nextInt();
+
+            }
+            while (board[row][col] != ' ');
+//            System.out.println("asd");
+            board[row][col] = player;
+
+        }
+        else{
+            board[row][col] = player;
+        }
+    }
+
+    public int[] invalid_move() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter row: ");
+        int new_row = scanner.nextInt();
+
+        System.out.println("Enter column: ");
+        int new_col = scanner.nextInt();
+        return new int []{new_row, new_col};
+    }
+
+    public void move_piece(int row, int col, int new_row, int new_col, char player) {
+        if (board[new_row][new_col] != ' ') {
+            do {
+                System.out.println("Position already occupied. Place piece elsewhere.");
+                new_row = invalid_move()[0];
+                new_col = invalid_move()[1];
+
+            }
+            while (board[new_row][new_col] != ' ');
+
+            if ((col % 2 == 0 &&
+                !(new_col == (col + 1) % 8 || new_col == (col - 1) % 8))) {
+                {
+                    do {
+                        System.out.println("Invalid move. Place piece elsewhere.");
+                        new_row = invalid_move()[0];
+                        new_col = invalid_move()[1];
+
+                    }
+                    while ((new_col != (new_col + 1) % 8) || (new_col != (new_col - 1) % 8) || (new_row != (new_row + 1) % 3) || (new_row != (new_row - 1) % 3));
+
+                }
+
+            }
+
+            else if ((col % 2 == 0) && (new_row != row)) {
+                do {
+                    System.out.println("Invalid move. Place piece elsewhere.");
+                    new_row = invalid_move()[0];
+                    new_col = invalid_move()[1];
+                }
+                while (new_row != row);
+
+            }
+
+            board[new_row][new_col] = player;
+            board[row][col] = ' ';
+
+        }
+    }
 }
