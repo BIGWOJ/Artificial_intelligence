791678ae838994222a823d657029a8e769b7c3d6
lw53894 <lw53894@zut.edu.pl>
2024-12-10 11:42:50 +0100

    Project finished

--- Naive Bayes Classifier/NBC_class.py
+++ Naive Bayes Classifier/NBC_class.py
@@ -5,7 +5,7 @@ class NBC(BaseEstimator, ClassifierMixin):
     def __init__(self, buckets_amount=None, laplace=False, logarithms=False):
         self.laplace = laplace
         self.logarithms = logarithms
-        self.classes_ = None
+        self.classes = None
         self.attributes_probabilities = None
         self.classes_probabilities = None
         self.buckets_amount = buckets_amount
@@ -14,83 +14,62 @@ class NBC(BaseEstimator, ClassifierMixin):
         discretized_X = []
         for attribute in range(X.shape[1]):
             data = X[:, attribute]
-            #print(self.buckets_amount)
             bin_size = (np.max(data) - np.min(data)) / self.buckets_amount
             for i in range(len(data)):
-                #print(int(min))
-               # exit(0)
                 bin_num = int((data[i] - np.min(data)) / bin_size)
                 if bin_num == self.buckets_amount:
                     bin_num -= 1
                 discretized_X.append(bin_num)
-            #discretized_X.append()
-        #
-        # for i in range(len(data)):
-        #     bin_num = int((data[i] - min) / bin_size)
-        #     if bin_num == num_bins:
-        #         bin_num -= 1
-        #     discretized_data.append(bin_num)
 
         return np.array(discretized_X).reshape(X.shape)
 
     def fit(self, X, y):
-        m, n = X.shape
-        if not np.issubdtype(X.dtype, np.integer):
-            X = self.discretize(X)
-            print(X)
+        self.classes, class_counts = np.unique(y, return_counts=True)
+        self.classes_probabilities = class_counts / len(self.classes)
 
-        self.classes_, class_counts = np.unique(y, return_counts=True)
-        K = len(self.classes_)
-        self.classes_probabilities = class_counts / m
-
-        # Tworzenie tablicy prawdopodobieństw warunkowych
         self.attributes_probabilities = []
-        for j in range(n):
+        for j in range(X.shape[1]):
             attribute_probabilities = []
             for value in np.unique(X[:, j]):
                 value_probabilities = []
-                for class_index, class_label in enumerate(self.classes_):
+                for class_index, class_label in enumerate(self.classes):
                     count = np.sum((X[:, j] == value) & (y == class_label))
+                    #P(A) ~= (k+1) / m + q -> PDF
                     if self.laplace:
                         count += 1
                         denominator = class_counts[class_index] + len(np.unique(X[:, j]))
                     else:
                         denominator = class_counts[class_index]
+
                     value_probabilities.append(count / denominator)
                 attribute_probabilities.append(value_probabilities)
             self.attributes_probabilities.append(np.array(attribute_probabilities))
+
         return self
 
     def predict_proba(self, X):
-        m, n = X.shape
-        K = len(self.classes_)
-        eps = 1e-300  # Zapobieganie zerowym prawdopodobieństwom
+        samples_amount, attributes_amount = X.shape
+        epsilon = 1e-100
 
-        # Obliczanie prawdopodobieństw dla każdej klasy
-        if not self.logarithms:
-            probabilities = np.ones((m, K))
-            for i in range(m):
-                for y_index in range(K):
-                    for j in range(n):
+        if self.logarithms:
+            probabilities = np.zeros((samples_amount, len(self.classes)))
+            for i in range(samples_amount):
+                for y_index in range(len(self.classes)):
+                    probabilities[i, y_index] += np.log(max(self.classes_probabilities[y_index], epsilon))
+                    for j in range(attributes_amount):
                         value_index = np.where(np.unique(X[:, j]) == X[i, j])[0][0]
-                        probabilities[i, y_index] *= max(self.attributes_probabilities[j][value_index, y_index], eps)
-                    probabilities[i, y_index] *= max(self.classes_probabilities[y_index], eps)
+                        probabilities[i, y_index] += np.log(max(self.attributes_probabilities[j][value_index, y_index], epsilon))
+
         else:
-            probabilities = np.zeros((m, K))
-            for i in range(m):
-                for y_index in range(K):
-                    probabilities[i, y_index] += np.log(max(self.classes_probabilities[y_index], eps))
-                    for j in range(n):
+            probabilities = np.ones((samples_amount, len(self.classes)))
+            for i in range(samples_amount):
+                for y_index in range(len(self.classes)):
+                    for j in range(attributes_amount):
                         value_index = np.where(np.unique(X[:, j]) == X[i, j])[0][0]
-                        probabilities[i, y_index] += np.log(max(self.attributes_probabilities[j][value_index, y_index], eps))
+                        probabilities[i, y_index] *= max(self.attributes_probabilities[j][value_index, y_index], epsilon)
+                    probabilities[i, y_index] *= max(self.classes_probabilities[y_index], epsilon)
 
-            # Konwersja log-prawdopodobieństw na rzeczywiste prawdopodobieństwa
-            probabilities -= probabilities.max(axis=1, keepdims=True)
-            probabilities = np.exp(probabilities)
-
-        # Normalizacja wyników
-        probabilities /= probabilities.sum(axis=1, keepdims=True)
         return probabilities
 
     def predict(self, X):
-        return self.classes_[np.argmax(self.predict_proba(X), axis=1)]
+        return self.classes[np.argmax(self.predict_proba(X), axis=1)]
---
+++ Naive Bayes Classifier/Tests.py
@@ -0,0 +1,112 @@
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import NBC_class
+
+def discretize(data, num_bins, min, max):
+    bin_size = (max - min) / num_bins
+    discretized_data = []
+
+    for i in range(len(data)):
+        bin_num = int((data[i] - min) / bin_size)
+        if bin_num == num_bins:
+            bin_num -= 1
+        discretized_data.append(bin_num)
+
+    return np.array(discretized_data)
+
+def wine():
+    wine_data = np.genfromtxt('wine.data', delimiter=',')
+    X = wine_data[:, 1:]
+    y = wine_data[:, -1]
+
+    mean_accuracy = 0
+    mean_accuracy_laplace = 0
+    mean_accuracy_logs = 0
+    mean_accuracy_laplace_logs = 0
+    tests_number = 10
+    bucket_amount = 2
+
+    for _ in range(tests_number):
+        X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+        discretized_wine_data = []
+        for attribute in range(X_train.shape[1]):
+            data = X_train[:, attribute]
+            discretized_wine_data.append(discretize(data, bucket_amount, np.min(data), np.max(data)))
+
+        X_train = np.array(discretized_wine_data).T
+        y_train = discretize(y_train, bucket_amount, np.min(y), np.max(y))
+        y_test = discretize(y_test, bucket_amount, np.min(y), np.max(y))
+
+        X_test_discretized = []
+        for attribute in range(X_test.shape[1]):
+            data = X_test[:, attribute]
+            X_test_discretized.append(discretize(data, bucket_amount, np.min(data), np.max(data)))
+        X_test = np.array(X_test_discretized).T
+
+        NBC = NBC_class.NBC(buckets_amount=bucket_amount)
+        NBC.fit(X_train, y_train)
+        y_pred = NBC.predict(X_test)
+        mean_accuracy += accuracy_score(y_test, y_pred)
+
+        NBC_laplace = NBC_class.NBC(laplace=True,  buckets_amount=bucket_amount)
+        NBC_laplace.fit(X_train, y_train)
+        y_pred = NBC_laplace.predict(X_test)
+        mean_accuracy_laplace += accuracy_score(y_test, y_pred)
+
+        NBC_logs = NBC_class.NBC(logarithms=True, buckets_amount=bucket_amount)
+        NBC_logs.fit(X_train, y_train)
+        y_pred = NBC_logs.predict(X_test)
+        mean_accuracy_logs += accuracy_score(y_test, y_pred)
+
+        NBC_laplace_logs = NBC_class.NBC(laplace=True, logarithms=True, buckets_amount=bucket_amount)
+        NBC_laplace_logs.fit(X_train, y_train)
+        y_pred = NBC_laplace_logs.predict(X_test)
+        mean_accuracy_laplace_logs += accuracy_score(y_test, y_pred)
+
+    print(f'{"=" * 10} Wine {"=" * 10}\nPredictions accuracy: {mean_accuracy / tests_number:.3f}')
+    print(f'Predictions accuracy using Laplace: {mean_accuracy_laplace / tests_number:.3f}')
+    print(f'Predictions accuracy using logarithms: {mean_accuracy_logs / tests_number:.3f}')
+    print(f'Predictions accuracy using Laplace and logarithms: {mean_accuracy_laplace_logs / tests_number:.3f}\n')
+
+def lenses():
+    lenses_data = np.genfromtxt('lenses.data', dtype=int)
+
+    X = lenses_data[:, 1:]
+    y = lenses_data[:, -1]
+
+    mean_accuracy = 0
+    mean_accuracy_laplace = 0
+    mean_accuracy_laplace_logs = 0
+    mean_accuracy_logs = 0
+    tests_number = 50
+    bucket_amount = 3
+
+    for _ in range(tests_number):
+        X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+        NBC = NBC_class.NBC(buckets_amount=bucket_amount)
+        NBC.fit(X_train, y_train)
+        y_pred = NBC.predict(X_test)
+        mean_accuracy += accuracy_score(y_test, y_pred)
+
+        NBC_laplace = NBC_class.NBC(laplace=True, buckets_amount=bucket_amount)
+        NBC_laplace.fit(X_train, y_train)
+        y_pred = NBC_laplace.predict(X_test)
+        mean_accuracy_laplace += accuracy_score(y_test, y_pred)
+
+        NBC_logs = NBC_class.NBC(logarithms=True, buckets_amount=bucket_amount)
+        NBC_logs.fit(X_train, y_train)
+        y_pred = NBC_logs.predict(X_test)
+        mean_accuracy_logs += accuracy_score(y_test, y_pred)
+
+        NBC_laplace_logs = NBC_class.NBC(laplace=True, logarithms=True, buckets_amount=bucket_amount)
+        NBC_laplace_logs.fit(X_train, y_train)
+        y_pred = NBC_laplace_logs.predict(X_test)
+        mean_accuracy_laplace_logs += accuracy_score(y_test, y_pred)
+
+    print(f'{"="*10} Contact lenses {"="*10}\nPredictions accuracy: {mean_accuracy / tests_number:.3f}')
+    print(f'Predictions accuracy using Laplace: {mean_accuracy_laplace / tests_number:.3f}')
+    print(f'Predictions accuracy using logarithms: {mean_accuracy_logs / tests_number:.3f}')
+    print(f'Predictions accuracy using Laplace and logarithms: {mean_accuracy_laplace_logs / tests_number:.3f}\n')
