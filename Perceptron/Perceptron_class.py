04e75525179500227b5f6714029bc2f60c120458
BIGWOJ <wojtek.latos@interia.pl>
2024-12-20 22:21:11 +0100

    Start of the perceptron algorithm project. Doing research

---
+++ Perceptron/Perceptron_class.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Perceptron():
+    def __init__(self, m, learning_rate=0.01):
+        self.m = m
+        self.learning_rate = learning_rate
+        self.weights = None
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
